4d0b8bc60c4b3ac42b3d7f5e3e9b354b8d0f635b
Ha Duy Trung <haduytrung@gmail.com>
2013-06-28 18:03:46 +0800

    Update web to handle facebook login success page

--- web.js
+++ web.js
@@ -1,2 +1,22 @@
-var connect = require('connect');
-connect.createServer(connect.static('/var/www/kinkie.im/public/')).listen(80);
+var connect = require('connect'), http = require('http'), https = require('https');
+var app = connect()
+    .use(connect.static('public/'))
+    .use(connect.query())
+    .use(function(req, res){
+        console.log(req.query);
+        if (req._parsedUrl.pathname == '/success') {
+            var url = "https://graph.facebook.com/oauth/access_token?client_id=550033468382419&redirect_uri=http://kinkie.local:8081/success&client_secret=1a9edcc9fd1dd56894eab6c93eaff41f&code=" + req.query.code;
+            https.get(url, function(fbRes) {
+                fbRes.on('data', function (chunk) {
+                    res.writeHead(302, {
+                      'Location': req.headers.referer + '?' + chunk
+                    });
+                    res.end();
+                    // res.end(chunk);
+                });
+            }).on('error', function(e) {
+                console.log("Got error: " + e.message);
+            });
+        }
+    });
+http.createServer(app).listen(80);
