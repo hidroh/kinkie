e0de91b49dc9a5a334a1a40b492a181cf4e553e7
Ha Duy Trung <haduytrung@gmail.com>
2013-06-28 20:57:46 +0800

    Fix aws https access error

--- web.js
+++ web.js
@@ -6,10 +6,13 @@ var app = connect()
         console.log(req.query);
         if (req._parsedUrl.pathname == '/success') {
             var url = "https://graph.facebook.com/oauth/access_token?client_id=461663813917591&redirect_uri=http://kinkie.im/success&client_secret=97f7a1fc314700b1ca7a905385e7d06f&code=" + req.query.code;
-            https.get(url, function(fbRes) {
+            var options = require('url').parse(url);
+            options.rejectUnauthorized = false;
+            options.agent = new https.Agent(options);
+            https.get(options, function(fbRes) {
                 fbRes.on('data', function (chunk) {
                     res.writeHead(302, {
-                      'Location': req.headers.referer + '?' + chunk
+                      'Location': 'local:///select.html?' + chunk
                     });
                     res.end();
                     // res.end(chunk);
