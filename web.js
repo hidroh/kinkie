efa3bbb3b5a992ddac29f71c6614774b7a7a45a0
Ha Duy Trung <haduytrung@gmail.com>
2013-06-28 18:12:07 +0800

    Change redirect url to production

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = connect()
     .use(function(req, res){
         console.log(req.query);
         if (req._parsedUrl.pathname == '/success') {
-            var url = "https://graph.facebook.com/oauth/access_token?client_id=550033468382419&redirect_uri=http://kinkie.local:8081/success&client_secret=1a9edcc9fd1dd56894eab6c93eaff41f&code=" + req.query.code;
+            var url = "https://graph.facebook.com/oauth/access_token?client_id=550033468382419&redirect_uri=http://kinkie.im/success&client_secret=1a9edcc9fd1dd56894eab6c93eaff41f&code=" + req.query.code;
             https.get(url, function(fbRes) {
                 fbRes.on('data', function (chunk) {
                     res.writeHead(302, {
